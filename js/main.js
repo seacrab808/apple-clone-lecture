f61561754de7bf02cf1521d6dea30fb9c1332efb
seacrab808 <seacrab808@naver.com>
2023-07-31 18:15:00 +0900

    Create: Apple 사이트 클론 강의

---
+++ js/main.js
@@ -0,0 +1,74 @@
+
+// 장바구니 드롭다운
+const basketStarterEl = document.querySelector('header .basket-starter')
+const basketEl = basketStarterEl.querySelector('.basket')
+
+
+basketStarterEl.addEventListener('click', function(event) {
+  event.stopPropagation()  //이벤트 전파 멈추기
+  if(basketEl.classList.contains('show')) {  //contains <= 포함 여부 확인
+    hideBasket()
+  } else {
+    showBasket()
+  }
+})
+basketEl.addEventListener('click', function(event){
+  event.stopPropagation()
+})
+
+window.addEventListener('click', function () {
+  hideBasket()
+})
+
+
+function showBasket() {
+  basketEl.classList.add('show')
+}
+function hideBasket() {
+  basketEl.classList.remove('show')
+}
+ 
+
+// 검색
+const headerEl = document.querySelector('header')
+
+const headerMenuEls = [...headerEl.querySelectorAll('ul.menu > li')]
+
+const searchWrapEl = headerEl.querySelector('.search-wrap')
+const searchStarterEl = headerEl.querySelector('.search-starter')
+const searchCloserEl = searchWrapEl.querySelector('.search-closer')
+const searchShadowEl = searchWrapEl.querySelector('.shadow')
+const searchInputEl = searchWrapEl.querySelector('input')
+const searchDelayEls = [...searchWrapEl.querySelectorAll('li')]
+
+searchStarterEl.addEventListener('click', showSearch)
+searchCloserEl.addEventListener('click',  hideSearch)
+searchShadowEl.addEventListener('click',  hideSearch)
+
+
+//검색 - 애니메이션 딜레이 효과
+function showSearch() {
+  headerEl.classList.add('searching')
+  document.documentElement.classList.add('fixed') //html 찾기
+  headerMenuEls.reverse().forEach(function(el, index) {
+    el.style.transitionDelay = index * .4 / headerMenuEls.length + 's'
+  })
+  searchDelayEls.forEach(function(el, index) {
+    el.style.transitionDelay = index * .4 / searchDelayEls.length + 's'
+  })
+  setTimeout(function() {
+    searchInputEl.focus()
+  }, 600)
+}
+function hideSearch() {
+  headerEl.classList.remove('searching')
+  document.documentElement.classList.remove('fixed')
+  headerMenuEls.reverse().forEach(function(el, index) {
+    el.style.transitionDelay = index * .4 / headerMenuEls.length + 's'
+  })
+  searchDelayEls.reverse().forEach(function(el, index) {
+    el.style.transitionDelay = index * .4 / searchDelayEls.length + 's'
+  })
+  searchDelayEls.reverse()
+  searchInputEl.value = ""
+}
