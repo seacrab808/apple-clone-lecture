6499a04e8557c7538d9cc112f63b6ecc3bae69f1
seacrab808 <seacrab808@naver.com>
2023-08-08 11:52:31 +0900

    Add: 25강 거침없는 성능 섹션까지 마무리

--- js/main.js
+++ js/main.js
@@ -1,74 +1,85 @@
-
 // 장바구니 드롭다운
-const basketStarterEl = document.querySelector('header .basket-starter')
-const basketEl = basketStarterEl.querySelector('.basket')
+const basketStarterEl = document.querySelector("header .basket-starter");
+const basketEl = basketStarterEl.querySelector(".basket");
 
-
-basketStarterEl.addEventListener('click', function(event) {
-  event.stopPropagation()  //이벤트 전파 멈추기
-  if(basketEl.classList.contains('show')) {  //contains <= 포함 여부 확인
-    hideBasket()
+basketStarterEl.addEventListener("click", function (event) {
+  event.stopPropagation(); //이벤트 전파 멈추기
+  if (basketEl.classList.contains("show")) {
+    //contains <= 포함 여부 확인
+    hideBasket();
   } else {
-    showBasket()
+    showBasket();
   }
-})
-basketEl.addEventListener('click', function(event){
-  event.stopPropagation()
-})
+});
+basketEl.addEventListener("click", function (event) {
+  event.stopPropagation();
+});
 
-window.addEventListener('click', function () {
-  hideBasket()
-})
-
+window.addEventListener("click", function () {
+  hideBasket();
+});
 
 function showBasket() {
-  basketEl.classList.add('show')
+  basketEl.classList.add("show");
 }
 function hideBasket() {
-  basketEl.classList.remove('show')
+  basketEl.classList.remove("show");
 }
- 
 
 // 검색
-const headerEl = document.querySelector('header')
+const headerEl = document.querySelector("header");
 
-const headerMenuEls = [...headerEl.querySelectorAll('ul.menu > li')]
+const headerMenuEls = [...headerEl.querySelectorAll("ul.menu > li")];
 
-const searchWrapEl = headerEl.querySelector('.search-wrap')
-const searchStarterEl = headerEl.querySelector('.search-starter')
-const searchCloserEl = searchWrapEl.querySelector('.search-closer')
-const searchShadowEl = searchWrapEl.querySelector('.shadow')
-const searchInputEl = searchWrapEl.querySelector('input')
-const searchDelayEls = [...searchWrapEl.querySelectorAll('li')]
+const searchWrapEl = headerEl.querySelector(".search-wrap");
+const searchStarterEl = headerEl.querySelector(".search-starter");
+const searchCloserEl = searchWrapEl.querySelector(".search-closer");
+const searchShadowEl = searchWrapEl.querySelector(".shadow");
+const searchInputEl = searchWrapEl.querySelector("input");
+const searchDelayEls = [...searchWrapEl.querySelectorAll("li")];
 
-searchStarterEl.addEventListener('click', showSearch)
-searchCloserEl.addEventListener('click',  hideSearch)
-searchShadowEl.addEventListener('click',  hideSearch)
-
+searchStarterEl.addEventListener("click", showSearch);
+searchCloserEl.addEventListener("click", hideSearch);
+searchShadowEl.addEventListener("click", hideSearch);
 
 //검색 - 애니메이션 딜레이 효과
 function showSearch() {
-  headerEl.classList.add('searching')
-  document.documentElement.classList.add('fixed') //html 찾기
-  headerMenuEls.reverse().forEach(function(el, index) {
-    el.style.transitionDelay = index * .4 / headerMenuEls.length + 's'
-  })
-  searchDelayEls.forEach(function(el, index) {
-    el.style.transitionDelay = index * .4 / searchDelayEls.length + 's'
-  })
-  setTimeout(function() {
-    searchInputEl.focus()
-  }, 600)
+  headerEl.classList.add("searching");
+  document.documentElement.classList.add("fixed"); //html 찾기
+  headerMenuEls.reverse().forEach(function (el, index) {
+    el.style.transitionDelay = (index * 0.4) / headerMenuEls.length + "s";
+  });
+  searchDelayEls.forEach(function (el, index) {
+    el.style.transitionDelay = (index * 0.4) / searchDelayEls.length + "s";
+  });
+  setTimeout(function () {
+    searchInputEl.focus();
+  }, 600);
 }
 function hideSearch() {
-  headerEl.classList.remove('searching')
-  document.documentElement.classList.remove('fixed')
-  headerMenuEls.reverse().forEach(function(el, index) {
-    el.style.transitionDelay = index * .4 / headerMenuEls.length + 's'
-  })
-  searchDelayEls.reverse().forEach(function(el, index) {
-    el.style.transitionDelay = index * .4 / searchDelayEls.length + 's'
-  })
-  searchDelayEls.reverse()
-  searchInputEl.value = ""
+  headerEl.classList.remove("searching");
+  document.documentElement.classList.remove("fixed");
+  headerMenuEls.reverse().forEach(function (el, index) {
+    el.style.transitionDelay = (index * 0.4) / headerMenuEls.length + "s";
+  });
+  searchDelayEls.reverse().forEach(function (el, index) {
+    el.style.transitionDelay = (index * 0.4) / searchDelayEls.length + "s";
+  });
+  searchDelayEls.reverse();
+  searchInputEl.value = "";
 }
+
+// 요소의 가시성 관찰
+const io = new IntersectionObserver(function (entries) {
+  entries.forEach(function (entry) {
+    if (!entry.isIntersecting) {
+      return;
+    }
+    entry.target.classList.add("show");
+  });
+});
+
+const infoEls = document.querySelectorAll(".info");
+infoEls.forEach(function (el) {
+  io.observe(el);
+});
