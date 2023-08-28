bf530de99abd8e06b9301cbb670b84b211e84de6
seacrab808 <seacrab808@naver.com>
2023-08-28 17:48:14 +0900

    20230828 43강까지 완강, 데스크탑 구현 완료

--- js/main.js
+++ js/main.js
@@ -1,3 +1,6 @@
+import ipads from "../data/ipads.js";
+import navigations from "../data/navigations.js";
+
 // 장바구니 드롭다운
 const basketStarterEl = document.querySelector("header .basket-starter");
 const basketEl = basketStarterEl.querySelector(".basket");
@@ -100,3 +103,61 @@ pauseBtn.addEventListener("click", function () {
   playBtn.classList.remove("hide");
   pauseBtn.classList.add("hide");
 });
+
+// '당신에게 맞는 iPad는?' 렌더링!
+const itemsEl = document.querySelector("section.compare .items");
+ipads.forEach(function (ipad) {
+  const itemEl = document.createElement("div");
+  itemEl.classList.add("item");
+
+  let colorList = "";
+  ipad.colors.forEach(function (color) {
+    colorList += /*html*/ `<li style="background-color: ${color};"></li>`;
+  });
+
+  itemEl.innerHTML = /*html*/ `
+    <div class="thumbnail">
+      <img src="${ipad.thumbnail}" alt="${ipad.name}" />
+    </div>
+    <ul class="colors">
+      ${colorList}
+    </ul>
+    <h3 class="name">${ipad.name}</h3>
+    <p class="tagline">${ipad.tagline}</p>
+    <p class="price">₩ ${ipad.price.toLocaleString("en-US")} 부터</p>
+    <button class="btn">구입하기</button>
+    <a href="${ipad.url}" class="link">더 알아보기</a>
+  `;
+
+  itemsEl.append(itemEl);
+});
+
+// navigations
+const navigationsEl = document.querySelector("footer .navigations");
+
+navigations.forEach(function (nav) {
+  const mapEl = document.createElement("div");
+  mapEl.classList.add("map");
+
+  let mapList = "";
+  nav.maps.forEach(function (map) {
+    mapList += /*html*/ `<li>
+      <a href="${map.url}">${map.name}</a>
+    </li>`;
+  });
+
+  mapEl.innerHTML = /*html*/ `
+    <h3>
+      <span class="text">${nav.title}</span>
+    </h3>
+    <ul>
+      ${mapList}
+    </ul>
+  `;
+
+  navigationsEl.append(mapEl);
+});
+
+// 연도 표시
+const thisYearEl = document.querySelector("span.this-year");
+thisYearEl.textContent = new Date().getFullYear();
